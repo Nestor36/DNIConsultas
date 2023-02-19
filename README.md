d7960aada741ed173d4027f95bdfe958562b49f3
Nestor36 <62574741+Nestor36@users.noreply.github.com>
2023-02-18 20:34:31 -0500

    18/02/2023
    solo gózalo 7u7

--- README.md
+++
@@ -1,2 +0,0 @@
-# DNIConsultas
-Developer : Nestor Ramos | Realizado en HTML, CS, JS y PHP. No me hago responsable por el mal uso que se le brinde, Código totalmente creado por mí, no brindo soporte.. idk
---
+++ api.php
@@ -0,0 +1,20 @@
+<?php
+$urls = array(
+  'http://sdv.midis.gob.pe/Sis_Comunidad/Comunidad/AjaxReniecDatosPersona?In_DNI='.$_GET['dni'],
+  'http://sdv.midis.gob.pe/Sis_IDM_Admin/Persona/GetRENIEC?iCodAplicacion=27&iIdTipDocumento=1&vNroDocumento='.$_GET['dni']
+);
+
+for ($i=0; $i < 2;) {
+  $Headers  = array(
+    'Accept: */*',
+    'Host: sdv.midis.gob.pe',
+    'Upgrade-Insecure-Requests: 1',
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
+  );
+  $CurlGET = curl_init($urls[$i]);
+  curl_setopt($CurlGET, CURLOPT_POSTFIELDS, false);
+  curl_setopt($CurlGET, CURLOPT_HTTPHEADER, $Headers);
+  $response = curl_exec($CurlGET);
+  $i++;
+}
+?>
